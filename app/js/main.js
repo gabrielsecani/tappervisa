896da279124f5ef7099359ddc8f026fab74b4b34
Paula Faria <paulafaria@Paulas-MacBook-Pro.local>
2018-02-01 21:51:38 -0200

    index contato merge

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,21 @@
-$('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 300
-    edge: 'left', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true // Choose whether you can drag to open on touch screens,
-  }
-);
+// $('.button-collapse').sideNav({
+//     menuWidth: 300, // Default is 300
+//     edge: 'left', // Choose the horizontal origin
+//     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//     draggable: true // Choose whether you can drag to open on touch screens,
+//   }
+// );
+
+var scan = document.getElementById('scan');
+var dataShow = document.getElementById('data-show');
+var loading = document.getElementById('loading');
+
+scan.addEventListener('click', function(e){
+  e.preventDefault();
+  loading.classList.add('active');
+  this.classList.add('hide');
+  setTimeout(function(){
+    loading.classList.remove('active');
+    dataShow.classList.add('active')
+  }, 2500)
+})
